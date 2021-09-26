b26dca9331effc39fff2022be83f30c9842eb84a
Josef Koch <josef.koch97@gmail.com>
2021-09-26 12:05:30 +0200

    refactoring

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,6 +19,7 @@ export class GildedRose {
 
     private readonly maxQuality = 50;
     private readonly minQuailty = 0;
+    private readonly sellInDeadline = 0;
 
     constructor(items = [] as Array<Item>) {
         this.items = items;
@@ -27,29 +28,10 @@ export class GildedRose {
     updateQuality(): Array<Item> {
         this.items.forEach(item => {
             switch (item.name) {
-                case GildedRose.AgedBrie: {
-                    this.increaseQuality(item, 1);
-                    item.sellIn--;
-                    break;
-                }
-                case GildedRose.BackstagePass: {
-                    if (item.sellIn <= 0) {
-                        item.quality = 0;
-                    } else if (item.sellIn <= 5) {
-                        this.increaseQuality(item, 3);
-                    } else if (item.sellIn <= 10) {
-                        this.increaseQuality(item, 2);
-                    } else {
-                        this.increaseQuality(item, 1);
-                    }
-                    item.sellIn--;
-                    break;
-                }
+                case GildedRose.AgedBrie: this.updateAgedBrie(item); break;
+                case GildedRose.BackstagePass: this.updateBackstagePass(item); break;
                 case GildedRose.Sulfuras: break;
-                default: {
-                    item.sellIn <= 0 ? this.decreaseQuality(item, 2) : this.decreaseQuality(item, 1);
-                    item.sellIn--;
-                }
+                default: this.updateUnkownItem(item);
             }
         });
 
@@ -62,6 +44,29 @@ export class GildedRose {
 
     private decreaseQuality(item: Item, by: number): void {
         item.quality = item.quality > by - 1 ? item.quality - by : this.minQuailty;
+    }
+
+    private updateAgedBrie(item: Item): void {
+        this.increaseQuality(item, 1);
+        item.sellIn--;
+    }
+
+    private updateBackstagePass(item: Item): void {
+        if (item.sellIn <= this.sellInDeadline) {
+            item.quality = 0;
+        } else if (item.sellIn <= 5) {
+            this.increaseQuality(item, 3);
+        } else if (item.sellIn <= 10) {
+            this.increaseQuality(item, 2);
+        } else {
+            this.increaseQuality(item, 1);
+        }
+        item.sellIn--;
+    }
+
+    private updateUnkownItem(item: Item): void {
+        item.sellIn <= this.sellInDeadline ? this.decreaseQuality(item, 2) : this.decreaseQuality(item, 1);
+        item.sellIn--;
     }
 
 }
